5b229553736ee8b402efd41970e3d09120458852
gampang123 <gampangrozaki@gmail.com>
2025-03-14 02:56:10 +0700

    kedua

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,10 @@
+</main>
+<!-- End Main Content -->
+
+<!-- Footer -->
+<footer class="text-center mt-auto">
+    <p>Copyright &copy; {{ date('Y') }} Lembah FItness Gym</p>
+</footer>
+
+</body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="icon" type="image/png" href="{{ asset('favicon.png') }}">
+
+        <title>Lembah Fitness Gym</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+
+        <!-- Styles -->
+        <script src="https://unpkg.com/@tailwindcss/browser@4"></script>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+    </head>
+    <body class="pl-8 pr-8 flex flex-col min-h-screen">
+        @include('layouts.header')
+
+        @section('title', 'Dashboard Page')
+
+        <h1>Lembah Fitness gym</h1>
+        <p>Ini adalah halaman utama.</p>
+
+        @include('layouts.footer')
+    </body>
+</html>
